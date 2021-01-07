b3acbcf7855b0120c545da84a6a8c556c0f41d14
Zekhniddin <zexjumayev@gmail.com>
2021-01-07 15:59:22 +0500

    Initial Commit

---
+++ UI-Task1Tests/UI_Task1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  UI_Task1Tests.swift
+//  UI-Task1Tests
+//
+//  Created by Зехниддин on 07/01/21.
+//
+
+import XCTest
+@testable import UI_Task1
+
+class UI_Task1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
